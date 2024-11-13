a35fb8acab38ea71af3ceb8c3acb46ad4f292996
JD Tadlock <jt7903@gmail.com>
2024-11-13 14:57:41 -0500

    added client example code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
 // import { useState } from 'react'
+import {useQuery, gql} from '@apollo/client';
+
+const GETALLPOSTS = gql`
+  query GetAllPosts {
+    getAllPosts {
+      _id
+      body
+      title
+      pet {
+        name
+      }
+    }
+  }
+`;
 
 function App() {
+  const {data, error} = useQuery(GETALLPOSTS);
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (data) {
+    console.log(data);
+  }
 
   return (
     <>
-     
+      {data && data.getAllPosts.map((post: any) => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+          <p>Added By: {post.pet.name}</p>
+        </div>
+      ))}
     </>
   )
 }
