0c59c89d694de03207f933792023ddb50e86a006
JD Tadlock <jt7903@gmail.com>
2024-11-21 16:35:03 -0500

    fixed deploy code

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173",
+    baseUrl: `http://localhost:${process.env.PORT}`,
   },
 
   component: {
