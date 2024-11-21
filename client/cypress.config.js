1a36e852ccb93ef1d71d1679b72680b21f9f42e4
JD Tadlock <jt7903@gmail.com>
2024-11-21 16:40:18 -0500

    fixed deploy code

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -4,6 +4,14 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('after:run', (results) => {
+        // Exit the process with the appropriate exit code
+        if (results.totalFailed > 0) {
+          process.exit(1); // Exit with a non-zero code if there are test failures
+        } else {
+          process.exit(0); // Exit with a zero code if all tests pass
+        }
+      });
     },
     baseUrl: `http://localhost:${process.env.PORT}`,
   },
